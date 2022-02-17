96b5bd107757f2c0b6c73c96365abc15953d9f03
Kaiyrgali Bekkali <kairgalib@ya.ru>
2022-02-17 13:51:22 +0500

    start optimize functions

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ ymaps.ready(function () {
   // let hrefIcon = '';
   var myMap = new ymaps.Map("YMapsID", {
     center: [40.71, -74],
-    zoom: 11,
+    zoom: 10,
 });
 
   bottons.addEventListener('click', (e)=>choiseMethod(e.target));
@@ -20,7 +20,53 @@ ymaps.ready(function () {
       // let hrefIcon = (choise.id=='btnFetch')?'../img/markRed.svg':'';
       if (choise.id=='btnXML') {
         hrefIcon = '../img/markRed.svg';
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.responseType = 'json';
+        console.log(xhr);
+        xhr.send(); //Зачем это здесь ?
+        xhr.onload = function() {
+          if (xhr.status != 200) {
+            alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Заменить на собственный обработчик ошибок
+          } else {
+            let stations = xhr.response.data.stations;
+            function getRandom (arr) {
+              return arr[Math.floor(Math.random() * arr.length)];
+            };
+            let activStations = [];
+            for (let i = 0; i < numLabels; i++) {
+              activStations.push(getRandom(stations));
+            } 
+            // console.log(activStations[0]);
+            let i=0;
+            let timerId = setInterval(() => {
+             var myPlacemark = new ymaps.Placemark([activStations[i].lat, activStations[i].lon], {
+               hintContent: `Станция номер - ${activStations[i].legacy_id} `,
+               balloonContent: `Название станции - ${activStations[i].name}`,
+             },
+             {
+              iconLayout: 'default#image',
+              iconImageHref: hrefIcon,
+              iconImageSize: [40, 40],
+              iconImageOffset: [0, 0]
+             });
+             
+             myMap.geoObjects.add(myPlacemark);
+             i++;
+            }, 1000);
+            setTimeout(() => clearInterval(timerId), numLabels*1000);
+            console.log('Finish');
+
+            // Если рандомизация и Обработка карты повторяется. Поэтому можно вывести ее в отдельную функцию и попробовать импортировать
+            }
+          };
+         
+
         
+        xhr.onerror = function() { // Заменить на свой обработчик ошибок
+          alert("Запрос не удался");
+        };
+
       } else if (choise.id=='btnFetch') {
         hrefIcon = '../img/markGreen.svg';
         async function getResponse () {
@@ -29,16 +75,16 @@ ymaps.ready(function () {
             if (myResponse.statusText == 'OK') {
               let dataBase = await myResponse.json();
               // console.log(dataBase);
-              console.log(Date(dataBase.last_updated)); /// добавить вывод обновления и общего количества станций
+              // console.log(Date(dataBase.last_updated)); /// добавить вывод обновления и общего количества станций
               
               let stations = dataBase.data.stations;
               // console.log(stations);
-              function rrr (arr) {
+              function getRandom (arr) {
                 return arr[Math.floor(Math.random() * arr.length)];
               };
              let activStations = [];
              for (let i = 0; i < numLabels; i++) {
-               activStations.push(rrr(stations));
+               activStations.push(getRandom(stations));
              } 
              console.log(activStations[0]);
              let i=0;
@@ -93,12 +139,55 @@ ymaps.ready(function () {
                 
       } else if (choise.id=='btnPromise') {
         hrefIcon = '../img/markBlue.svg';
-      } else return
-    
-      
-      
-     };
-      });
+        let i = 0;
+        function loadStaitions (i) {
+          return new Promise ((resolve) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.responseType = 'json';
+            console.log(xhr);
+            xhr.send();
+            xhr.onload = function() {
+            let stations = xhr.response.data.stations;
+            function getRandom (arr) {
+              return arr[Math.floor(Math.random() * arr.length)];
+            };
+            let activStations = getRandom(stations); //сократил, так как массив не нужен
+            // console.log(activStations[0]);
+                       
+             var myPlacemark = new ymaps.Placemark([activStations.lat, activStations.lon], { //немного отличается, то что не массив
+               hintContent: `Станция номер - ${activStations.legacy_id} `,
+               balloonContent: `Название станции - ${activStations.name}`,
+             },
+             {
+              iconLayout: 'default#image',
+              iconImageHref: hrefIcon,
+              iconImageSize: [40, 40],
+              iconImageOffset: [0, 0]
+             });
+             
+             myMap.geoObjects.add(myPlacemark);
+            
+            console.log('Finish');
+            setTimeout(()=> resolve(), 1000)}
+          })
+               
+        }
+        loadStaitions (1)
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+          .then (() => {return loadStaitions (1)})
+
+          // catch (alert ('Mistake!');
+      } else return };
+    }
+      );
 
       
 
