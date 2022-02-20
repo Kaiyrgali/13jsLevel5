53201daefa2a99114a7cfe362ae7d77979a2a9d9
Kaiyrgali Bekkali <kairgalib@ya.ru>
2022-02-20 22:47:00 +0500

    before install ESLint

--- js/script.js
+++ js/script.js
@@ -1,85 +1,96 @@
 "use strict"
 
-const buttons = document.querySelector('#buttons');
-const errorBtn = document.querySelector('#btnError');
-const spanStatus = document.querySelector('#statusId');
-let url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
-const maxStations = 10;
-let myMap = new Object ();
-let stations = new Array;
-let activeBtn = new Object();
-let activStations = new Array;
-let iconMap = new String;
+let url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json',
+    myMap = new Object(),
+    stations = new Array(),
+    activeBtn = new Object(),
+    activStations = new Array(),
+    iconMap = new String();
 
-function getRandom (arr) {
+const maxStations = 10,
+      buttons = document.querySelector('#buttons'),
+      errorBtn = document.querySelector('#btnError'),
+      spanStatus = document.querySelector('#statusId'),
+      activeBtnClass = "btn-active",
+      errorBtnClass = "error-active",
+      errorText = "Возникла ошибка. Попробуйте позже ...",
+      errorGenerateText = "Cгенерирована ошибка",
+      errorNotText = "Ошибки не должно быть",
+      errorRequestText= "Запрос не удался",
+      errorLoadMapText = "Карта не загрузилась. Проверьте соединение с интернетом ...",
+      LoadMapOk = "Карта загружена",
+      urlWrong = "some wrong url",
+      urlRight = url,
+      mapCenter = [40.71, -74],
+      mapZoom = 10;
+
+function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)]
 };
 
-function changeBtnStyle (choise) {
+function changeBtnStyle(choise) {
   for (let i = 0; i < buttons.children.length; i++) {
-  buttons.children[i].classList.remove("btn-active");
-};
-  choise.classList.add("btn-active");
+    buttons.children[i].classList.remove(activeBtnClass);
+  }
+  choise.classList.add(activeBtnClass);
 };
 
-function errorActiv () {
-  errorBtn.classList.toggle('error-active');
-  if (errorBtn.classList == 'error-active') {
-    newStatus ('сгенерирована ошибка');
-    return url='some wrong url';
+function changeErrorStyle() {
+  errorBtn.classList.toggle(errorBtnClass);
+  if (errorBtn.classList == errorBtnClass) {
+    newStatus (errorGenerateText);
+    url = urlWrong;
   } else {
-    newStatus ('ошибки не должно быть');
-    return url= 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
+    newStatus (errorNotText);
+    url = urlRight;
   }
 };
 
-function newStatus (comment) {
+function newStatus(comment) {
   spanStatus.innerText = comment;
 }
 
-function makeStations () {
-  console.log(stations);
-  activStations=[];
-    for (let i = 0; i < maxStations; i++) {
-      activStations.push(getRandom(stations));
-    }
+function makeStations() {
+  activStations = [];
+  for (let i = 0; i < maxStations; i++) {
+    activStations.push(getRandom(stations));
   }
+}
 
-function drawStations () {
+function drawStations() {
   myMap.geoObjects.removeAll();
   for (let i = 0; i < activStations.length; i++) {
     let myPlacemark = new ymaps.Placemark(
-      [activStations[i].lat, activStations[i].lon], {
-        hintContent: `Станция номер - ${activStations[i].legacy_id} `,
-        balloonContent: `Название станции - ${activStations[i].name}`,
-      },
-      {preset: iconMap});
-      myMap.geoObjects.add(myPlacemark);
-      newStatus (`Показано ${(i+1)} станций из ${stations.length}`);
-    };
-};
+    [activStations[i].lat, activStations[i].lon], {
+    hintContent: `Станция номер - ${activStations[i].legacy_id} `,
+    balloonContent: `Название станции - ${activStations[i].name}`,
+    }, {preset: iconMap});
+    myMap.geoObjects.add(myPlacemark);
+    newStatus (`Показано ${(i + 1)} станций из ${stations.length}`);
+  }
+}
 
-ymaps.ready(newMap, newStatus ('Карта не загрузилась. Проверьте соединение с интернетом ...'));
+ymaps.ready(newMap, newStatus (errorLoadMapText));
 
 function newMap() {
-  myMap = new ymaps.Map("YMapsID", {center: [40.71, -74], zoom: 10,});
-  newStatus('карта загружена');
+  myMap = new ymaps.Map("YMapsID", {center: mapCenter, zoom: mapZoom});
+  newStatus(LoadMapOk);
 };
 
-errorBtn.addEventListener('click', errorActiv);
+errorBtn.addEventListener('click', changeErrorStyle);
+
 buttons.addEventListener('click', (e)=>{
   activeBtn = e;
   changeBtnStyle (activeBtn.target); // меняем цвета активной кнопки
   choiseMethod(activeBtn.target) // скачиваем базу велосипедов одним из трех ассинхронных методов  
 });
 
-
-function choiseMethod (choise) {
-  if (choise.id=='btnXML') {
+function choiseMethod(choise) {
+  if (choise.id == 'btnXML') {
     methodOne ();
-  } else if (choise.id=='btnFetch') {
-  methodTwo ();
-  } else if (choise.id=='btnPromise') {
+  } else if (choise.id == 'btnFetch') {
+    methodTwo ();
+  } else if (choise.id == 'btnPromise') {
     methodThree ()
       .then (() => methodThree ())
       .then (() => methodThree ())
@@ -90,30 +101,31 @@ function choiseMethod (choise) {
       .then (() => methodThree ())
       .then (() => methodThree ())
       .then (() => methodThree ())
-      .catch (newStatus ('Возникла ошибка. Попробуйте позже ...'))
-  } else return };
+      .catch (newStatus (errorText))
+  } else return
+};
 
-function methodOne () {
+function methodOne() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.responseType = 'json';
   xhr.send();
   xhr.onload = function() {
     if (xhr.status != 200) {
-     newStatus ('Возникла ошибка. Попробуйте позже ...');
+      newStatus (errorText);
     } else {
       iconMap = 'islands#blueBicycleIcon';
       stations = xhr.response.data.stations;
       makeStations () // Генерируем массив из 10ти (maxNumber) станций
       drawStations (); // Рисуем на карте 10ть (maxNumber) станций        
     }
-    xhr.onerror = function() {
-      newStatus("Запрос не удался");
-    };
   };
+  xhr.onerror = function() {
+    newStatus(errorRequestText);
+  }
 }; 
 
-async function methodTwo () {
+async function methodTwo() {
   const myResponse = await fetch (url);
   if (myResponse.statusText == 'OK') {
     const dataBase = await myResponse.json();
@@ -121,22 +133,22 @@ async function methodTwo () {
     stations = dataBase.data.stations;
     makeStations () // Генерируем массив из 10ти (maxNumber) станций
     drawStations (); // Рисуем на карте 10ть (maxNumber) станций        
-  } else newStatus ('Возникла ошибка. Попробуйте позже ...');          
+  } else newStatus (errorText);          
 };
 
-function methodThree () {
+function methodThree() {
   return new Promise ((resolve) => {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.responseType = 'json';
     xhr.send();
     xhr.onload = function() {
-    iconMap = 'islands#greenBicycleIcon';
-    stations = xhr.response.data.stations;
-    makeStations (); // Генерируем массив из 10ти (maxNumber) станций
-    drawStations (); // Рисуем на карте 10ть (maxNumber) станций     
-    console.log('methodThree');
-    resolve ();
-  }
+      iconMap = 'islands#greenBicycleIcon';
+      stations = xhr.response.data.stations;
+      makeStations (); // Генерируем массив из 10ти (maxNumber) станций
+      drawStations (); // Рисуем на карте 10ть (maxNumber) станций     
+      console.log('methodThree');
+      resolve ();
+    }
   })
 }
