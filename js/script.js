30170ce88328aab520076697cf9b95f63c9b388c
Kaiyrgali Bekkali <kairgalib@ya.ru>
2022-02-20 18:07:09 +0500

    secondVersion

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@
 const buttons = document.querySelector('#buttons');
 const errorBtn = document.querySelector('#btnError');
 const spanStatus = document.querySelector('#statusId');
-let hrefIcon = '';
 let url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
-let maxStations = 10;
-const activStations = [];
-let counter = 0;
+const maxStations = 10;
+let myMap = new Object ();
+let stations = new Array;
+let activeBtn = new Object();
+let activStations = new Array;
+let iconMap = new String;
 
 function getRandom (arr) {
   return arr[Math.floor(Math.random() * arr.length)]
@@ -15,8 +17,8 @@ function getRandom (arr) {
 
 function changeBtnStyle (choise) {
   for (let i = 0; i < buttons.children.length; i++) {
-    buttons.children[i].classList.remove("btn-active");   
-  }
+  buttons.children[i].classList.remove("btn-active");
+};
   choise.classList.add("btn-active");
 };
 
@@ -35,146 +37,106 @@ function newStatus (comment) {
   spanStatus.innerText = comment;
 }
 
-const myMap = () => {new ymaps.Map("YMapsID", {
-    center: [40.71, -74],
-    zoom: 10,
-  });
-  newStatus('карта загружена');}
+function makeStations () {
+  console.log(stations);
+  activStations=[];
+    for (let i = 0; i < maxStations; i++) {
+      activStations.push(getRandom(stations));
+    }
+  }
 
+function drawStations () {
+  myMap.geoObjects.removeAll();
+  for (let i = 0; i < activStations.length; i++) {
+    let myPlacemark = new ymaps.Placemark(
+      [activStations[i].lat, activStations[i].lon], {
+        hintContent: `Станция номер - ${activStations[i].legacy_id} `,
+        balloonContent: `Название станции - ${activStations[i].name}`,
+      },
+      {preset: iconMap});
+      myMap.geoObjects.add(myPlacemark);
+      newStatus (`Показано ${(i+1)} станций из ${stations.length}`);
+    };
+};
 
-ymaps.ready(myMap, newStatus ('Карта не загрузилась. Проверьте соединение с интернетом ...')); // Либо тут колбеки и зис
+ymaps.ready(newMap, newStatus ('Карта не загрузилась. Проверьте соединение с интернетом ...'));
 
+function newMap() {
+  myMap = new ymaps.Map("YMapsID", {center: [40.71, -74], zoom: 10,});
+  newStatus('карта загружена');
+};
 
-console.dir (ymaps);
-console.log (myMap);
-
-
-let myPlacemark = (counter) => {
-  new ymaps.Placemark([activStations[counter].lat, activStations[counter].lon], {
-  hintContent: `Станция номер - ${activStations[counter].legacy_id} `,
-  balloonContent: `Название станции - ${activStations[counter].name}`,
-},{
-  iconLayout: 'default#image',
-  iconContent: 'islands#blueBicycleIcon',
-  iconImageSize: [40, 40],
-  iconImageOffset: [0, 0]
-});
-
-myMap.geoObjects.add(myPlacemark);
-}
-
-/*
 errorBtn.addEventListener('click', errorActiv);
 buttons.addEventListener('click', (e)=>{
-    myMap.geoObjects.removeAll();
-    counter = 0;
-    changeBtnStyle (e.target);
-    choiseMethod(e.target)});
-  
-  let choiseMethod = (choise) => {
-      if (choise.id=='btnXML') {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.responseType = 'json';
-        xhr.send();
-        xhr.onload = function() {
-          if (xhr.status != 200) {
-            newStatus ('Возникла ошибка. Попробуйте позже ...');
-          } else {
-            let stations = xhr.response.data.stations;
-            for (let i = 0; i < maxStations; i++) {
-              activStations.push(getRandom(stations));
-            }
-            let timerId = setInterval(() => {
-             const myPlacemark = new ymaps.Placemark([activStations[counter].lat, activStations[counter].lon], {
-               hintContent: `Станция номер - ${activStations[counter].legacy_id} `,
-               balloonContent: `Название станции - ${activStations[counter].name}`,
-             },
-             {
-              iconLayout: 'default#image',
-              iconImageHref: hrefIcon,
-              iconImageSize: [40, 40],
-              iconImageOffset: [0, 0]
-             });
-             
-             myMap.geoObjects.add(myPlacemark);
-             newStatus (`Показано ${(counter+1)} станций из ${stations.length}`);
-             counter++;
-            }, 1000);
-            setTimeout(() => clearInterval(timerId), maxStations*1000);
-            }
-          };
-        xhr.onerror = function() {
-          newStatus("Запрос не удался");
-        };
+  activeBtn = e;
+  changeBtnStyle (activeBtn.target); // меняем цвета активной кнопки
+  choiseMethod(activeBtn.target) // скачиваем базу велосипедов одним из трех ассинхронных методов  
+});
 
-      } else if (choise.id=='btnFetch') {
-        hrefIcon = '../img/markGreen.svg';
-        async function getResponse () {
-          let myResponse = await fetch (url);
-            if (myResponse.statusText == 'OK') {
-              let dataBase = await myResponse.json();
-              let stations = dataBase.data.stations;
-              for (let i = 0; i < maxStations; i++) {
-                activStations.push(getRandom(stations));
-                };
-             let timerId = setInterval(() => {
-              const myPlacemark = new ymaps.Placemark([activStations[counter].lat, activStations[counter].lon], {
-                hintContent: `Станция номер - ${activStations[counter].legacy_id} `,
-                balloonContent: `Название станции - ${activStations[counter].name}`,
-              },
-              {
-               iconLayout: 'default#image',
-               iconImageHref: hrefIcon,
-               iconImageSize: [40, 40],
-               iconImageOffset: [0, 0]
-              });
-              myMap.geoObjects.add(myPlacemark);
-              newStatus (`Показано ${(counter+1)} станций из ${stations.length}`);
-              counter++;
-             }, 1000);
-             setTimeout(() => clearInterval(timerId), maxStations*1000);    
-          } else newStatus ('Возникла ошибка. Попробуйте позже ...');          
-        };
-          getResponse();    
-      } else if (choise.id=='btnPromise') {
-        hrefIcon = '../img/markBlue.svg';
-        function loadStaitions () {
-          return new Promise ((resolve) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.responseType = 'json';
-            xhr.send();
-            xhr.onload = function() {
-            let stations = xhr.response.data.stations;
-            let activStations = getRandom(stations);//немного отличается, то что не массив
-            const myPlacemark = new ymaps.Placemark([activStations.lat, activStations.lon], { 
-               hintContent: `Станция номер - ${activStations.legacy_id} `,
-               balloonContent: `Название станции - ${activStations.name}`,
-             },
-             {
-              iconLayout: 'default#image',
-              iconImageHref: hrefIcon,
-              iconImageSize: [40, 40],
-              iconImageOffset: [0, 0]
-             });
-             myMap.geoObjects.add(myPlacemark);
-             newStatus (`Показано ${(counter+1)} станций из ${stations.length}`);
-             counter++;
-            setTimeout(()=> resolve(), 1000)}
-          })
-               
-        }
-        loadStaitions ()
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .then (() => loadStaitions ())
-          .catch (newStatus ('Возникла ошибка. Попробуйте позже ...'))
-      } else return };
-      */
+
+function choiseMethod (choise) {
+  if (choise.id=='btnXML') {
+    methodOne ();
+  } else if (choise.id=='btnFetch') {
+  methodTwo ();
+  } else if (choise.id=='btnPromise') {
+    methodThree ()
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .then (() => methodThree ())
+      .catch (newStatus ('Возникла ошибка. Попробуйте позже ...'))
+  } else return };
+
+function methodOne () {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+  xhr.send();
+  xhr.onload = function() {
+    if (xhr.status != 200) {
+     newStatus ('Возникла ошибка. Попробуйте позже ...');
+    } else {
+      iconMap = 'islands#blueBicycleIcon';
+      stations = xhr.response.data.stations;
+      makeStations () // Генерируем массив из 10ти (maxNumber) станций
+      drawStations (); // Рисуем на карте 10ть (maxNumber) станций        
+    }
+    xhr.onerror = function() {
+      newStatus("Запрос не удался");
+    };
+  };
+}; 
+
+async function methodTwo () {
+  const myResponse = await fetch (url);
+  if (myResponse.statusText == 'OK') {
+    const dataBase = await myResponse.json();
+    iconMap = 'islands#redBicycleIcon';
+    stations = dataBase.data.stations;
+    makeStations () // Генерируем массив из 10ти (maxNumber) станций
+    drawStations (); // Рисуем на карте 10ть (maxNumber) станций        
+  } else newStatus ('Возникла ошибка. Попробуйте позже ...');          
+};
+
+function methodThree () {
+  return new Promise ((resolve) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.send();
+    xhr.onload = function() {
+    iconMap = 'islands#greenBicycleIcon';
+    stations = xhr.response.data.stations;
+    makeStations (); // Генерируем массив из 10ти (maxNumber) станций
+    drawStations (); // Рисуем на карте 10ть (maxNumber) станций     
+    console.log('methodThree');
+    resolve ();
+  }
+  })
+}
